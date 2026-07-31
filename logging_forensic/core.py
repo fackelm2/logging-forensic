9527e568351a25bf7bf38e1ae8260566963af5a1
fackelm2 <git@nmit.de>
2026-07-31 15:16:17 +0200

    update log formatter in `forensic_logger` to include filename

--- logging_forensic/core.py
+++ logging_forensic/core.py
@@ -67,7 +67,7 @@ def forensic_logger(name: str,
             print(f'forensic-logger: creating logger {name}')
         file_handler = logging.FileHandler(logfile_path)
         file_handler.setLevel(level)
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
