8cd7a34ecdeb34a68f1f717ae9ba247551c31240
fackelm2 <git@nmit.de>
2026-07-31 15:06:45 +0200

    clean up and reorganize docstring in `forensic_logger` for clarity

--- logging_forensic/core.py
+++ logging_forensic/core.py
@@ -1,32 +1,4 @@
-"""A utility function to create and configure a customizable forensic logger.
 
-This function enables the creation of a logger with various options such as
-logging to a specified file, setting a logging level, enabling console
-output, appending timestamps to log filenames, and verbosity for debugging
-purposes. The logger is designed to streamline file-based logging in
-structured applications with minimal configuration.
-
-:param name: The name of the logger.
-:type name: str
-:param logfile_path: The path to the log file. Defaults to a generated file
-    inside the "log" directory within the current working directory.
-:type logfile_path: Path, optional
-:param level: The logging level for the logger, which may be specified as
-    a string or `logging` level constant. Defaults to `logging.INFO`.
-:type level: typing.Union[str, int], optional
-:param console: If True, a console handler is added for logging to the
-    standard output. Defaults to False.
-:type console: bool, optional
-:param timestamp: If True, a timestamped filename is generated for the log
-    file. Defaults to False.
-:type timestamp: bool, optional
-:param verbose: Enables verbose console debugging output during the logger
-    setup process. Defaults to False.
-:type verbose: bool, optional
-
-:return: A configured logger instance.
-:rtype: logging.Logger
-"""
 import logging
 from typing import Union, Optional
 from pathlib import Path
@@ -38,14 +10,36 @@ def forensic_logger(name: str,
                  console: bool = False,
                  timestamp: bool = False,
                  verbose: bool = False) -> logging.Logger:
+    """A utility function to create and configure a customizable forensic logger.
 
-    """
+    This function enables the creation of a logger with various options such as
+    logging to a specified file, setting a logging level, enabling console
+    output, appending timestamps to log filenames, and verbosity for debugging
+    purposes. The logger is designed to streamline file-based logging in
+    structured applications with minimal configuration.
+
+    :param name: The name of the logger.
+    :type name: str
+    :param logfile_path: The path to the log file. Defaults to a generated file
+        inside the "log" directory within the current working directory.
+    :type logfile_path: Path, optional
+    :param level: The logging level for the logger, which may be specified as
+        a string or `logging` level constant. Defaults to `logging.INFO`.
+    :type level: typing.Union[str, int], optional
+    :param console: If True, a console handler is added for logging to the
+        standard output. Defaults to False.
+    :type console: bool, optional
+    :param timestamp: If True, a timestamped filename is generated for the log
+        file. Defaults to False.
+    :type timestamp: bool, optional
+    :param verbose: Enables verbose console debugging output during the logger
+        setup process. Defaults to False.
+    :type verbose: bool, optional
+
+    :return: A configured logger instance.
+    :rtype: logging.Logger
+
     create and setup a logger with path "../log/",optional timestamp and optional console logging
-    :param name: name of the logger
-    :param logfile_path: path to log file
-    :param level: logging level
-    :param console: if true console logging
-    :return: logger object
     """
 
     if isinstance(level, str):
